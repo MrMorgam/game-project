97580706e73bd66d22b1c03c356f53386c3a579f
mrmorgam <gabriel.am.mor@gmail.com>
2024-02-25 11:00:23 -0300

    Creating basic scenes and the main player

---
+++ player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const SPEED = 500.0
+
+func _physics_process(delta):
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0 , SPEED)
+
+	move_and_slide()
---
+++ settings_menu.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
